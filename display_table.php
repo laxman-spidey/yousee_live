b1741db2126705a9f1fea27e0bb7dd8a599fa25b
laxman <mittu.thefire@gmail.com>
2013-02-14 01:20:48 +0530

    source format

--- display_table.php
+++ display_table.php
@@ -5,24 +5,26 @@
 $i = 0;
 echo "<table style='font-size:12px'>";
 echo "<thead><tr>";
-while ($i < mysql_num_fields($result)){
-echo "<th bgcolor=\"CCCCFF\">". mysql_field_name($result, $i) . "</th>";
-$i++;
+while ($i < mysql_num_fields($result))
+{
+	echo "<th bgcolor=\"CCCCFF\">". mysql_field_name($result, $i) . "</th>";
+	$i++;
 }
 echo "</thead></tr>";
 
 //display the data
 $i = 1;
-while ($rows = mysql_fetch_array($result,MYSQL_ASSOC)){
-// variable for coloring oddeven rows
-$oddeven = $i & 1;
-if ($oddeven == 0){$color = "CCCFFF";}
-else {$color = "FFFFFF";}
+while ($rows = mysql_fetch_array($result,MYSQL_ASSOC))
+{
+	// variable for coloring oddeven rows
+	$oddeven = $i & 1;
+	if ($oddeven == 0){$color = "CCCFFF";}
+	else {$color = "FFFFFF";}
 
-echo "<tr bgcolor=\"$color\">";
-foreach ($rows as $data){echo "<td align=\"right\">".$data ."</td>";}
-echo "</tr>";
-$i++;
+	echo "<tr bgcolor=\"$color\">";
+	foreach ($rows as $data){echo "<td align=\"right\">".$data ."</td>";}
+	echo "</tr>";
+	$i++;
 }
 
 echo "</table>";

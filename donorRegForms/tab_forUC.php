d338472a6296c8299c187ffa4afa0a6aac5a7fc7
laxman <mittu.thefire@gmail.com>
2013-02-14 00:37:04 +0530

    SOURCE FORMATTING DONE

--- donorRegForms/tab_forUC.php
+++ donorRegForms/tab_forUC.php
@@ -1,38 +1,42 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-
-<body>
-
-
-<table border="0">
-  <tr>
-    <td>
-      <label>Feature Permission <span class="link"><a href="javascript: void(0)"><font face=verdana,arial,helvetica size=2>[?]</font><span>For featuring your photo and Quote on YouSee website. We request you to select "YES"</span></a></span></label>
-    </td>
-    <td>    <label><input type="radio" name="featurePermission" value="Y" id="FeaturePermission_y" />
-        yes</label>
-
-      <label>
-        <input type="radio" checked name="featurePermission" value="N" id="FeaturePermission_n" />
-        no </label>
-      
-    </td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td style="vertical-align:top;"><label for="quote">Quote <span class="link"><a href="javascript: void(0)"><font face=verdana,arial,helvetica size=2>[?]</font><span>Please show a brief (1-3 lines) quote about your thoughts on volunteer,donations and overall about UC (max 300 characters)</span></a></span></label></td>
-    <td><span style="vertical-align:top;">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Untitled Document</title>
+	</head>
+	<body>
+		<table border="0">
+			<tr>
+				<td>
+					<label>Feature Permission <span class="link"><a href="javascript: void(0)"><font face=verdana,arial,helvetica size=2>[?]</font><span>For featuring your photo and Quote on YouSee website. We request you to select "YES"</span>
+						</a>
+						</span>
+					</label>
+				</td>
+				<td>
+					<label>
+						<input type="radio" name="featurePermission" value="Y" id="FeaturePermission_y"
+						/>yes</label>
+					<label>
+						<input type="radio" checked name="featurePermission" value="N" id="FeaturePermission_n"
+						/>no</label>
+				</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td style="vertical-align:top;">
+					<label for="quote">Quote <span class="link"><a href="javascript: void(0)"><font face=verdana,arial,helvetica size=2>[?]</font><span>Please show a brief (1-3 lines) quote about your thoughts on volunteer,donations and overall about UC (max 300 characters)</span>
+						</a>
+						</span>
+					</label>
+				</td>
+				<td><span style="vertical-align:top;">
       <textarea placeholder="Please show a brief (1-3 lines) quote about your thoughts on volunteer,donations and overall about UC" name="featureQuote" id="quote" cols="45" rows="5"></textarea>
-    </span></td>
-    <td>&nbsp;</td>
-  </tr>
+    </span>
+				</td>
+				<td>&nbsp;</td>
+			</tr>
+		</table>
+	</body>
 
-</table>
-
-
-</body>
 </html>
--- header_navbar.php
+++ header_navbar.php
@@ -1,32 +1,21 @@
 <!--header-->
 <div id="header">
-
-<!-- UC logo -->
-<img style="float:left;" src="images/uc-logo.jpg">
-
-<!-- login and register -->
-<div style="float:right;">
-<?php 
-if(!isset($_SESSION['SESS_USER_ID'])){
-include 'login_form.php';
-}
-else{
-include 'display_donor_info.php';
-echo "<td>&nbsp;</td>";
-echo "<a href=\"logout.php\">Logout</a>";
-}
-?>
+	<!-- UC logo -->
+	<img style="float:left;" src="images/uc-logo.jpg">
+	<!-- login and register -->
+	<div style="float:right;">
+		<?php 
+			if(!isset($_SESSION[ 'SESS_USER_ID']))
+			{ 
+				include 'login_form.php'; 
+			} 
+			else
+			{
+				include 'display_donor_info.php'; echo "<td>&nbsp;</td>"; echo "<a href=\"logout.php\">Logout</a>"; 
+			} 
+		?>
+	</div>
 </div>
-
-</div>
-
-
 <!--navbar-->
-<?php 
-if(!isset($_SESSION['SESS_USER_ID'])){
-include 'navbar.php';
-}
-else{
-include 'navbar_myuc.php';
-}
-?>
+<?php if(!isset($_SESSION[ 'SESS_USER_ID'])){ include 'navbar.php'; } else{ include
+'navbar_myuc.php'; } ?>
